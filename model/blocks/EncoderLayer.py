69cb9ef4ceb51cd9234c5c5590f4a5ab3f3aff62
Youssef-Bahaa <youssefbahaa755@gmail.com>
2026-06-15 23:47:23 +0300

    Implemented Encoder Layer

---
+++ model/blocks/EncoderLayer.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from model.attention.multihead_attention import MultiHeadAttention
+from feedforward import FeedForward
+
+class EncoderLayer(nn.Module):
+    def __init__(self, embed_dim, num_heads, ff_dim):
+        super.__init__()
+
+        self.attn = MultiHeadAttention(embed_dim, num_heads)
+        self.norm1 = nn.LayerNorm(embed_dim)
+
+        self.ff = FeedForward(embed_dim, ff_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        attn_out = self.attn(self.attn)
+        x = self.norm1(x + self.dropout(attn_out))
+
+        ffn_out = self.ff(x)
+        self.norm2(x + self.dropout(ffn_out))
+
+        return x
---
+++ model/blocks/encoder.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+import torch
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
