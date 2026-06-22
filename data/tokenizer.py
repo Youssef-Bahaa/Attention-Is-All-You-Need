002b8871cdb9034fe1a553e09dc2398f8d38c7f3
Youssef-Bahaa <youssefbahaa755@gmail.com>
2026-06-22 20:49:04 +0300

    fixed tokenizer

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -2,11 +2,13 @@ from collections import Counter
 
 class Vocab:
     def __init__(self, counter, min_freq=2):
-        PAD = '<pad>'
-        UNK = '<unk>'
-        SOS = '<sos>'
-        EOS = '<eos>'
-        self.SPECIALS = [PAD, UNK, SOS, EOS]
+        self.PAD = '<pad>'
+        self.UNK = '<unk>'
+        self.SOS = '<sos>'
+        self.EOS = '<eos>'
+
+
+        self.SPECIALS = [self.PAD, self.UNK, self.SOS, self.EOS]
 
         self.token2idx = {tok : i for i,tok in enumerate(self.SPECIALS)}
         self.idx2token = list(self.SPECIALS)
@@ -16,8 +18,13 @@ class Vocab:
                 self.token2idx[token] = len(self.idx2token)
                 self.idx2token.append(token)
 
+        self.PAD_IDX = self.token2idx[self.PAD]
+        self.UNK_IDX = self.token2idx[self.UNK]
+        self.SOS_IDX = self.token2idx[self.SOS]
+        self.EOS_IDX = self.token2idx[self.EOS]
 
-    def len(self):
+
+    def __len__(self):
         return len(self.token2idx)
 
     def encode(self, sentence: str):
@@ -25,19 +32,19 @@ class Vocab:
         indices = [self.token2idx.get(t, self.UNK_IDX)  for t in tokens]
         return indices
 
-    def decoder(self, indices):
+    def decode(self, indices):
         tokens = [self.idx2token[i] for i in indices
                   if i not in (self.PAD_IDX, self.SOS_IDX, self.EOS_IDX)]
 
         return " ".join(tokens)
 
 
-def build_vocab(self, hf_dataset, min_freq=2):
+def build_vocab(hf_dataset, min_freq=2):
         en_counter, de_counter = Counter(), Counter()
         for row in hf_dataset['train']:
             for tok in row['en'].lower().split():
                 en_counter[tok] += 1
-            for tok in row['en'].lower().split():
+            for tok in row['de'].lower().split():
                 de_counter[tok] += 1
 
         return Vocab(en_counter, min_freq), Vocab(de_counter, min_freq)
